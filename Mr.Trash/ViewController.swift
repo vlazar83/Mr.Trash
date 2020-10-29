05508e61154401565fbb2fe20ebe0af94283706f
admin <admin@admins-mbp.t.hu>
2020-10-29 14:18:05 +0100

    initial setup

---
+++ Mr.Trash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mr.Trash
+//
+//  Created by admin on 2020. 10. 29..
+//  Copyright © 2020. admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Mr.Trash' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Mr.Trash
+  pod 'SwipeableTabBarController'
+  pod 'paper-onboarding'
+
+end
